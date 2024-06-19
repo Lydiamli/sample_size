a624b1c37cdf2da4ff894dd5a5f14557fe7a0b56
Lydia <83355334+Lydiamli@users.noreply.github.com>
2024-06-18 22:56:07 -0400

    t-test

--- calculate_N.R
+++ calculate_N.R
@@ -29,3 +29,37 @@ for (beta in betas) {
 
 # show results
 results
+
+
+
+# Define a function 
+calculate_sample_size_t_test <- function(effect_size, alpha, power = 0.80) {
+    power_result <- pwr.t.test(d = effect_size, 
+                               sig.level = alpha, 
+                               power = power, 
+                               type = "two.sample")
+    N_per_group <- ceiling(power_result$n)  # Ensure N is rounded up to the nearest whole number
+    total_N <- N_per_group * 2  # total sample size for two groups
+    return(total_N)
+}
+
+# Define parameters
+effect_size <- 0.6
+alpha_levels <- c(0.05, 0.025, 0.01)
+power <- 0.80
+
+# Store results
+t_test_results <- data.frame(alpha = numeric(), 
+                             N = numeric())
+
+# Apply function and loop through each alpha level
+for (alpha in alpha_levels) {
+    total_N <- calculate_sample_size_t_test(effect_size, alpha, power)
+    t_test_results <- rbind(t_test_results, data.frame(alpha = alpha, N = total_N))
+}
+
+
+t_test_results
+
+
+
